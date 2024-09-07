9c962f3ef7ec22fd9e152a2ba4f3d0d200a62ce3
Huy Cao <77295189+kics223w1@users.noreply.github.com>
2024-09-07 21:16:35 +0700

    Task 1 (#1)
    * refactor: done logic task 1
    
    * fix: word doesn't break on mobile screen

--- src/components/MiddleBlock/styles.tsx
+++ src/components/MiddleBlock/styles.tsx
@@ -14,6 +14,11 @@ export const MiddleBlockSection = styled("section")`
 
 export const Content = styled("p")`
   padding: 0.75rem 0 0.75rem;
+  word-wrap: break-word;
+  max-width: 100%;
+  overflow-wrap: break-word;
+  word-break: break-word;
+  hyphens: auto;
 `;
 
 export const ContentWrapper = styled("div")`
